39a082d3f8c27a0f26547d613ce9be66694c379e
CarlR <carl.rudroff@gmail.com>
2014-09-25 20:01:52 -0500

    Fixed Account entity

--- StudentLotto/src/main/java/com/studentLotto/account/Account.java
+++ StudentLotto/src/main/java/com/studentLotto/account/Account.java
@@ -1,14 +1,11 @@
 package com.studentLotto.account;
 
-import java.security.MessageDigest;
-
 import javax.persistence.*;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(name = "account")
 @NamedQuery(name = Account.FIND_BY_EMAIL, query = "select a from Account a where a.email = :email")
 public class Account implements java.io.Serializable {
 
@@ -16,20 +13,24 @@ public class Account implements java.io.Serializable {
 
 	@Id
 	@GeneratedValue
-	private Long id;
+	private Long id; 
 
 	@Column(unique = true)
 	private String email;
-
+	
 	@JsonIgnore
 	private String password;
+	
+	@OneToOne
+	@JoinColumn(name="id", referencedColumnName="accountId")
+	private Person person;
 
 	private String role = "ROLE_USER";
 
-	protected Account() {
+    protected Account() {
 
 	}
-
+	
 	public Account(String email, String password, String role) {
 		this.email = email;
 		this.password = password;
@@ -40,7 +41,7 @@ public class Account implements java.io.Serializable {
 		return id;
 	}
 
-	public String getEmail() {
+    public String getEmail() {
 		return email;
 	}
 
@@ -64,28 +65,12 @@ public class Account implements java.io.Serializable {
 		this.role = role;
 	}
 
-	public boolean emailAccountActivation(String emailAddress, String password) {
-		return false;
+	public Person getPerson() {
+		return person;
 	}
 
-	public String generateAccountActivationKey(String emailAddress,
-			String password) {
-		String accountActivationkey = "";
-		return accountActivationkey;
-	}
-
-	public boolean isActivationKeyUnique(String activationKey) {
-		return false;
-	}
-
-	public boolean saveAccountActivationInfo(String activationKey,
-			long accountId) {
-		return false;
-	}
-
-	public boolean sendEmail(String recipientAddress, String emailTitle,
-			String emailBody) {
-		return false;
+	public void setPerson(Person person) {
+		this.person = person;
 	}
 
 }
