a69c203a59a2e3620dc91aee829e06a1960f2be0
CarlR <carl.rudroff@gmail.com>
2014-10-02 15:26:20 -0500

    University Package

---
+++ StudentLotto/src/main/java/com/studentLotto/university/UniversityRepository.java
@@ -0,0 +1,30 @@
+package com.studentLotto.university;
+
+import javax.persistence.*;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class UniversityRepository {
+	
+	@PersistenceContext
+	private EntityManager entityManager;	
+	
+	@Transactional
+	public University save(University university) {
+		entityManager.persist(university);
+		return university;
+	}
+	
+	public University findByUniversityId(int id) {
+		try {
+			return entityManager.createNamedQuery(University.FIND_BY_ID, University.class)
+					.setParameter("id", id)
+					.getSingleResult();
+		} catch (PersistenceException e) {
+			return null;
+		}
+	}
+}
